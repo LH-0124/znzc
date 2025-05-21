0549aa7b0f5fe1d0aa6f555ecacfc0b46d943d8e
LH-0124 <3230103696@zju.edu.cn>
2025-05-21 22:02:47 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -17,8 +17,33 @@ const questionBank = [
   },
   {
     audio: "/audio/3.mp3",
-    answer: "我们的项目是面向听觉障碍人群的语音识别康复系统",
+    answer: "浙江省的省会是杭州市，杭州最有名的景点是西湖",
     difficulty: 1
+  },
+   {
+    audio: "/audio/4.mp3",
+    answer: "生物医学工程专业在未来有着非常美好的前景",
+    difficulty: 1
+  },
+  {
+    audio: "/audio/5.mp3",
+    answer: "你好，可以过来帮我把我的书拿到这里吗？", 
+    difficulty: 2
+  },
+  {
+    audio: "/audio/6.mp3",
+    answer: "爷爷，我回来了，好久不见我好想你呀",
+    difficulty: 1
+  },
+  {
+    audio: "/audio/7.mp3",
+    answer: "过了很久终于我愿抬头看，你就在对岸走的好慢",
+    difficulty: 1
+  },
+  {
+    audio: "/audio/8.mp3",
+    answer: "你好，今天我给你准备了很好吃的晚餐", 
+    difficulty: 2
   }
 ];
 
