c7d7b7990a4666ddd9aab3363c338d1b153ba450
LH-0124 <3230103696@zju.edu.cn>
2025-05-22 18:06:40 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -6,44 +6,9 @@ let currentQuestion = null;
 
 const questionBank = [
   {
-    audio: "/audio/1.mp3",
-    answer: "这是一个面向听觉障碍人群的语音识别康复系统",
+    audio: "/audio/9.mp3",
+    answer: "我想想，我觉得这几天有点过的稀里糊涂，我已经忘了都怎么过的了",
     difficulty: 1
-  },
-  {
-    audio: "/audio/2.mp3",
-    answer: "生物医学工程学是一门理工医相结合的交叉学科", 
-    difficulty: 2
-  },
-  {
-    audio: "/audio/3.mp3",
-    answer: "浙江省的省会是杭州市，杭州最有名的景点是西湖",
-    difficulty: 1
-  },
-   {
-    audio: "/audio/4.mp3",
-    answer: "生物医学工程专业在未来有着非常美好的前景",
-    difficulty: 1
-  },
-  {
-    audio: "/audio/5.mp3",
-    answer: "你好，可以过来帮我把我的书拿到这里吗？", 
-    difficulty: 2
-  },
-  {
-    audio: "/audio/6.mp3",
-    answer: "爷爷，我回来了，好久不见我好想你呀",
-    difficulty: 1
-  },
-  {
-    audio: "/audio/7.mp3",
-    answer: "过了很久终于我愿抬头看，你就在对岸走的好慢",
-    difficulty: 1
-  },
-  {
-    audio: "/audio/8.mp3",
-    answer: "你好，今天我给你准备了很好吃的晚餐", 
-    difficulty: 2
   }
 ];
 
